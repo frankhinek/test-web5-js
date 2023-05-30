bd42e5c729a68c53db10f0c573ce7ee89300d50e
Frank Hinek <frankhinek@users.noreply.github.com>
2023-05-30 13:38:22 -0400

    Update c8 config
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/web5-user-agent/tests/example.spec.ts
+++ packages/web5-user-agent/tests/example.spec.ts
@@ -1,10 +1,15 @@
 import { expect } from 'chai';
 
-import { hello } from '../src/main.js';
+import { add, hello } from '../src/main.js';
 
 describe('Web5 User Agent', () => {
   it('works', () => {
     const result = hello();
     expect(result).to.equal('Hello, world!');
+  });
+
+  it('can add', () => {
+    const result = add(1, 2);
+    expect(result).to.equal(3);
   });
 });
